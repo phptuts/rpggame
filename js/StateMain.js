29aa4eeb7be4e4964f3e69556522704120a29f1b
phptuts <php.noah@gmail.com>
2016-11-23 01:02:02 -0600

    Got tile map inside phaser wip config

--- js/StateMain.js
+++ js/StateMain.js
@@ -2,12 +2,15 @@ var StateMain={
     
    preload:function()
     {
-       
+        game.load.tilemap('city', 'js/board.json', null, Phaser.Tilemap.TILED_JSON);
+        game.load.image('rogue_city', 'image/roguelikeCity_transparent.png');
+        game.load.image('tiles', 'image/RPGpack_sheet.png');
     },
     
     create:function()
     {
-        
+        map = game.add.tilemap('city');
+
     },
     
     update:function()
--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var game;
 window.onload = function()
 {
-    game=new Phaser.Game(1500,780,Phaser.AUTO,"ph_game");
+    game=new Phaser.Game(1500,750,Phaser.AUTO,"ph_game");
 	
     game.state.add("StateMain",StateMain);
     game.state.start("StateMain");
